e43ccfd6730a192c0b74908eb3b411496056e08b
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2013-03-08 20:21:26 +0100

    added twitter bootstrap included bootstrap css

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 
         <title><?php wp_title(); ?></title>
 
+        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/bootstrap/css/bootstrap.min.css" media="screen" type="text/css" />
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     </head>
